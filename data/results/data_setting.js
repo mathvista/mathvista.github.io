6ab24a68c5e67226e0c3dcbf11259d19a96b4568
lupantech <lupantech@gmail.com>
2023-10-03 15:26:18 -0700

    update functions

--- data/results/data_setting.js
+++ data/results/data_setting.js
@@ -1,15 +1,14 @@
 name_to_folder_map = {
-  "bard": "bard",
-  "chatgpt_pot": "chatgpt_pot",
-  "gpt4v": "gpt4v",
-  "minigpt4": "minigpt4",
+  "instruct_blip2_vicuna13b": "instruct_blip2_vicuna13b",
+  "idefics_9b_instruct": "idefics_9b_instruct",
+  "bard": "Bard",
   "chatgpt_cot": "chatgpt_cot",
-  "llama_adapter_v2": "llama_adapter_v2",
-  "gpt4_pot": "gpt4_pot",
+  "minigpt4": "minigpt4",
+  "llava_llama_2_13b": "llava_llama_2_13b",
+  "gpt4_cot": "gpt4_cot",
   "mplugowl_ft_7b": "mplugowl_ft_7b",
   "claude": "claude",
-  "gpt4_cot": "gpt4_cot",
-  "llava_llama_2_13b": "llava_llama_2_13b",
-  "idefics_9b_instruct": "idefics_9b_instruct",
-  "instruct_blip2_vicuna13b": "instruct_blip2_vicuna13b"
+  "llama_adapter_v2": "llama_adapter_v2",
+  "chatgpt_pot": "chatgpt_pot",
+  "gpt4_pot": "gpt4_pot"
 }
